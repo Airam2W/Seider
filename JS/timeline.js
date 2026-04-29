113603b0d0ebd97b68eeb1205618655cfb137957
Airam21170247 <olaairam@gmail.com>
2026-04-29 10:42:14 -0700

    Total Amount of all Entries in Timeline Interface added. Scroll to bottom button added in Timeline. Change logic of Entry adding Subtags on each Tag, notes, subtotals, blocks, take a photo/upload PDF, view of picture/PDF and download picture/PDF functions. Notes, subtags, subtotals, PDF/Picture urls added in DB.

--- JS/timeline.js
+++ JS/timeline.js
@@ -17,15 +17,17 @@ import {
 
 const db = getFirestore(app);
 const entriesContainer = document.getElementById("entriesContainer");
+const timelineEl = document.getElementById("timeline");
+const totalAmountEl = document.getElementById("totalAmount");
 
 let currentUser = null;
+
 
 // =======================
 // AUTH CHECK
 // =======================
 onAuthStateChanged(auth, async (user) => {
     currentUser = user;
-
     await loadEntries(user.uid);
 });
 
@@ -68,16 +70,24 @@ async function loadEntries(uid) {
         collection(db, "users", uid, "entries")
     );
 
+    const totalAmount = snapshot.docs.reduce((sum, docItem) => {
+        const data = docItem.data();
+        return sum + (data.total || 0);
+    }, 0);
+
+    totalAmountEl.textContent = `Total Amount: ${totalAmount}`;
+
     snapshot.forEach((docItem) => {
         const data = docItem.data();
         const entryId = docItem.id;
+        const entryTotal = data.total || 0;
 
         const div = document.createElement("div");
         div.className = "entry";
 
         div.innerHTML = `
-            <h3>Entry</h3>
             <p>${new Date(data.date.seconds * 1000).toLocaleString()}</p>
+            <p>Total: ${entryTotal}</p>
 
             <button onclick="viewEntry('${entryId}')">View / Edit</button>
             <button onclick="deleteEntry('${entryId}')">Delete</button>
@@ -107,8 +117,40 @@ window.deleteEntry = async (id) => {
         );
 
         loadEntries(currentUser.uid);
+        toggleFixedBottom();
 
     } catch (error) {
         console.error(error);
     }
 };
+
+
+// =======================
+// SCROLL TO BOTTOM
+// =======================
+document.getElementById("btnScrollBottom").onclick = () => {
+  window.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: "smooth"
+  });
+};
+
+
+const element = document.getElementById("totalAmountContainer");
+function toggleFixedBottom() {
+  const body = document.documentElement;
+  const hasScroll = body.scrollHeight > body.clientHeight;
+
+  if (hasScroll) {
+    element.classList.add("fixed-bottom");
+  } else {
+    element.classList.remove("fixed-bottom");
+  }
+}
+
+// Ejecutar al cargar
+window.addEventListener("load", toggleFixedBottom);
+// Ejecutar al cambiar tamaño
+window.addEventListener("resize", toggleFixedBottom);
+// Ejecutar al hacer scroll
+window.addEventListener("scroll", toggleFixedBottom);
